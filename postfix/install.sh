f1223e35029c5396029cb8b3e5ae6bff6bfeaba9
hovmand <jonas@hovmand.eu>
2017-07-27 19:49:30 +0200

    Changes cert paths and enables postfix TLS

--- postfix/install.sh
+++ postfix/install.sh
@@ -23,6 +23,19 @@ chmod +x /opt/postfix.sh
 postconf -e myhostname=${MAILNAME}
 postconf -e myorigin=${MAILNAME}
 
+postconf -e smtpd_tls_CAfile=/etc/certs/chain.pem
+postconf -e smtpd_tls_cert_file=/etc/certs/cert.pem
+postconf -e smtpd_tls_key_file=/etc/certs/key.pem
+
+postconf -e smtpd_tls_loglevel=1
+postconf -e smtpd_tls_received_header=yes
+postconf -e smtpd_tls_security_level=may
+postconf -e smtpd_use_tls=yes
+
+# When contacting other servers
+postconf -e smtp_tls_note_starttls_offer=yes
+postconf -e smtp_use_tls=yes
+
 cat > /etc/mailname <<EOF
 ${MAILNAME}
 EOF
