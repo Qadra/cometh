cbabf7b42abc0cc1ff31c087d485f00edfb2fe54
hovmand <jonas@hovmand.eu>
2017-07-27 19:33:05 +0200

    Fixes forwardings

--- db/layout/20-forwardings.sql
+++ db/layout/20-forwardings.sql
@@ -1,9 +1,7 @@
 CREATE TABLE `forwardings` (
 	  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	  `local` varchar(80) NOT NULL,
-	  `domain` int(10) unsigned NOT NULL,
+	  `source` varchar(255) NOT NULL,
+	  `destination` text NOT NULL,
 	  PRIMARY KEY (`id`),
-	  UNIQUE KEY `local` (`local`,`domain`),
-	  KEY `domain` (`domain`),
-	  CONSTRAINT `forwardings_ibfk_1` FOREIGN KEY (`domain`) REFERENCES `domains` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+	  KEY `source` (`source`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
